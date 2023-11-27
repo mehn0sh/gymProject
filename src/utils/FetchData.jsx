5413ad47a96b41f802f28eb9ff5b96450f1753b3
unknown <mahnooshekrami@gmail.com>
2023-11-27 22:31:29 +0330

    first commit

--- src/utils/FetchData.jsx
+++ src/utils/FetchData.jsx
@@ -8,6 +8,9 @@ headers: {
 };
 export const youtubeoptions = {
 method: 'GET',
+params: {
+  id: 'UCE_M8A5yxnLfW0KghEeajjw'
+},
 headers: {
   'X-RapidAPI-Key': '7e5dd87463msh677b0f8a2d56550p159038jsn7f5df81f6f40',
   'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
